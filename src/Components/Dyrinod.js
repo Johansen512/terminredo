1a3c927a2e9638c99799376ab6293f5fb04947fc
Johansen512 <tekstogtext@gmail.com>
2021-02-17 13:20:34 +0100

    mere fetch ... er det snart weekend

--- src/Components/Dyrinod.js
+++ src/Components/Dyrinod.js
@@ -1,15 +1,54 @@
 /**@jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { dataContext } from "../Contexts/DataContext";
+import {useContext, useEffect, useState} from "react";
 
 const Dyrinod = () => {
-    return ( 
+//Data hentet fra context
+const { dyrinoddata } = useContext(dataContext);
+
+dyrinoddata && console.log("from context:", dyrinoddata);
+
+const dyrinodstyle=css`
+    
+    top:1100px;
+   margin: 0 10px;
+    border: red solid 4px;
+    position:absolute;
+    height:200px;
+    width: 100%;
+    overflow:hidden;
+    `;
 
 
-      <div>
-        <h2>Står du med et dyr i nød?</h2>
 
-        <p>tjuf</p></div>
+const dyrinodtextstyle=css`
+margin:0;
+position: relative;
+left: 300px;
+z-index:10;
+color:white;
+
+`;
+
+
+
+
+
+    return dyrinoddata && ( 
+
+<>
+       <div css={dyrinodtextstyle}>
+        <h1>{dyrinoddata.title}</h1>
+       <p>{dyrinoddata.content}</p>
+        </div>
+
+        <div css={dyrinodstyle}>
+       <img  src={dyrinoddata.asset.url} alt="Sonic? Er det dig?" />
+
+       </div>
+       
+        </>
      );
 }
  
